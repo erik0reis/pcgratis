d1dd851e46f66e652ffc989dc92db25b45481734
erik0reis <58051404+erik0reis@users.noreply.github.com>
2022-11-14 11:25:53 -0300

    Update main.js

--- criarpc/main.js
+++ criarpc/main.js
@@ -1,11 +1,4 @@
-var connectionOptions =  {
-    "force new connection" : true,
-    "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
-    "timeout" : 10000, //before connect_error and connect_timeout are emitted.
-    "transports" : ["websocket"]
-};
-
-var socket = io("server.pcgratis.ga:342", connectionOptions);
+var socket = io("http://server.pcgratis.ga:342");
 
 var daypicker = document.getElementById('daypicker');
 var timepicker = document.getElementById('timepicker');
