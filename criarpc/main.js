3096252c7b7a58cb1f11a018c07d5c3f70c70a03
erik0reis <58051404+erik0reis@users.noreply.github.com>
2022-11-14 11:33:03 -0300

    Update main.js

--- criarpc/main.js
+++ criarpc/main.js
@@ -1,11 +1,4 @@
-var connectionOptions =  {
-    "force new connection" : true,
-    "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
-    "timeout" : 10000, //before connect_error and connect_timeout are emitted.
-    "transports" : ["websocket"]
-};
-
-var socket = io("187.105.108.61:342", connectionOptions);
+var socket = io("187.105.108.61:342");
 
 var daypicker = document.getElementById('daypicker');
 var timepicker = document.getElementById('timepicker');
