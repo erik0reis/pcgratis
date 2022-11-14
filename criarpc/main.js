bdb9476dffaad97b3878730efba9c7c70e694150
erik0reis <58051404+erik0reis@users.noreply.github.com>
2022-11-14 11:42:40 -0300

    Update main.js

--- criarpc/main.js
+++ criarpc/main.js
@@ -1,6 +1,6 @@
 var socket = io("http://187.105.108.61:342", {
   // WARNING: in that case, there is no fallback to long-polling
-  transports: [ "websocket" ]
+  transports: [ "polling" ]
 });
 
 var daypicker = document.getElementById('daypicker');
